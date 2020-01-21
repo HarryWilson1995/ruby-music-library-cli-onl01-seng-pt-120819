5ce94b0cd3aa235fcc4e8aaabd20acd8e92a1d2e
Harry Wilson <harry.wilson1995a@gmail.com>
2020-01-21 15:13:47 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -40,9 +40,7 @@ class MusicLibraryController
   end 
   
   def list_songs
-    songs_sorted_by_name = Song.all.sort_by! do |song|
-      song.name 
-    end 
+    songs_sorted_by_name = Song.all.sort {|a, b| a.name <=> b.name}
   binding.pry 
     songs_sorted_by_name.each.with_index(1) do |song, index|
       puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
